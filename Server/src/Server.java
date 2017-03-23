963cb1dd7e66475e5b77fb721e84d76d8f3e4a7e
Liam Brown <liamnb525@gmail.com>
2017-03-23 17:23:14 -0400

    NOT FUNCTIONAL changes for server user list

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -14,6 +15,9 @@ import java.util.Arrays;
  * @since 3/19/2017 : 2:15 PM
  */
 public class Server extends JFrame implements Runnable, ClientActionListener {
+    private UserData userList = new UserData();
+    File savedUserList = new File("Users.svs");
+
     private JTextArea serverLog;
     private JPanel panel;
     private JLabel numberOnlineLabel;
@@ -34,6 +38,10 @@ public class Server extends JFrame implements Runnable, ClientActionListener {
         this.setVisible(true);
         server = new ServerSocket(port);
         output("Local IP: " + InetAddress.getLocalHost());
+
+        //Load username list from file.
+        userList = loadUserData();
+
         this.run();
     }
 
@@ -47,6 +55,8 @@ public class Server extends JFrame implements Runnable, ClientActionListener {
                 c.addListener(this);
                 Thread t = new Thread(c);
                 t.start();
+                userList.users.add((new UserData()).instantiateUser(false, "h"));
+                writeUserData(userList);
             } catch (IOException e) {
                 output(Arrays.toString(e.getStackTrace()));
             }
@@ -91,4 +101,61 @@ public class Server extends JFrame implements Runnable, ClientActionListener {
         System.out.println(toOutput);
         updateLabel();
     }
+
+    /**
+     * Loads the list of username and statuses(UserData objects) into memory
+     * @return UserData[] An array of all UserData objects in the list
+     */
+    private UserData loadUserData(){
+        ObjectInputStream in = null;
+        UserData loaded = null;
+        if(!savedUserList.exists()){
+            System.err.println("Cannot load list, does not exist...");
+            return new UserData();
+        } else {
+            try {
+                in = new ObjectInputStream(new FileInputStream(savedUserList));
+                loaded = (UserData) in.readObject();
+            } catch (IOException e){
+                System.err.println("Could not open stream to read usernames into memory");
+                e.printStackTrace();
+            } catch(ClassNotFoundException e){
+                System.err.println("Could not load UserData object from file");
+            }
+            return loaded;
+        }
+    }
+
+    private void writeUserData(UserData toSave){
+        ObjectOutputStream out = null;
+        try {
+            out = new ObjectOutputStream(new FileOutputStream(savedUserList));
+            out.writeObject(toSave);
+        } catch (IOException e){
+            System.err.println("Could not open stream to write usernames into memory");
+        }
+    }
+
+    /**
+    Contains the server's knowledge of each user who has connected previously.
+     Will be written to file to persist indefinitely between multiple server launches.
+     **/
+    private class UserData implements Serializable{
+       ArrayList<User> users = new ArrayList<>();
+
+        private class User implements Serializable{
+            boolean isMod;
+            String userName;
+
+            public User(boolean isMod, String userName){
+                this.isMod = isMod;
+                this.userName = userName;
+            }
+        }
+
+        public User instantiateUser(boolean isMod, String userName){
+            return new User(isMod, userName);
+        }
+    }
+
 }
